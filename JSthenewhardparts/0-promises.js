de0a3143eb4928629976999e245c9585bdf3d3a6
huy75 <huy.nguyen@holbertonschool.com>
2022-01-05 12:18:33 +0000

    add some comments

--- JSthenewhardparts/0-promises.js
+++ JSthenewhardparts/0-promises.js
@@ -10,52 +10,49 @@ function sayHello() {
 // Uncomment the line below when ready
 sayHello(); // should log "Hello" after 1000ms
 
-
 // Challenge 2
 // Create a promise. Have it resolve with a value of 'Resolved!' in resolve after a delay of 1000ms, using setTimeout.
 // Print the contents of the promise after it has been resolved by passing console.log to .then
 var promise = new Promise(function (resolve, reject) {
   // ADD CODE HERE
-  setTimeout(resolve, 1000)
+  setTimeout(resolve, 1000);
 });
 
 // Should print out "Resolved!"
 // ADD CODE HERE
 promise.then(() => console.log("Resolved!"));
 
-
 // Challenge 3
 // Create another promise. Now have it reject with a value of "Rejected!" without using setTimeout.
 // Print the contents of the promise after it has been rejected by passing console.log to .catch
-promise = new Promise(function(resolve, reject) {
+promise = new Promise(function (resolve, reject) {
   // ADD CODE HERE
-  reject("Rejected!")
-})
+  reject("Rejected!");
+});
 
 // Should print out "Reject!"
 // ADD CODE HERE
-promise.catch(error => console.log(error))
-
+promise.catch((error) => console.log(error));
 
 // Challenge 4
 // Promises are asynchronous and we're now going to prove that they indeed are!
 // Create a promise and have it resolve with the value of "Promise has been resolved!"
 // Then uncomment the code at bottom of Challenge 4.
 // What order do we expect "Promise has been resolved!" and "I'm not the promise!" to print? Why?
+// the console.log comes first as promises go through the Microtask Queue
 promise = new Promise(function (resolve, reject) {
   // ADD CODE HERE
-  resolve('Promise has been resolved!');
+  resolve("Promise has been resolved!");
 });
 
 // Uncomment the lines below when ready
-promise.then(() => console.log('Promise has been resolved!'));
+promise.then(() => console.log("Promise has been resolved!"));
 console.log("I'm not the promise!");
-
 
 // Challenge 5
 // Write a function delay that returns a promise.
 // And that promise should return a setTimeout that calls resolve after 1000ms
-function delay(){
+function delay() {
   return new Promise((resolve, reject) => {
     setTimeout(() => resolve(), 1000);
   });
@@ -65,7 +62,6 @@ function delay(){
 // This code should log "Hello" after 1000ms
 // .then stores the sayHello function to be triggered when timer is outdated
 delay().then(sayHello);
-
 
 // Challenge 6
 // This challenge we'll chain promises together using ".then"
@@ -82,15 +78,16 @@ var firstPromise = new Promise((resolve, reject) => {
   resolve(secondPromise);
 });
 
-firstPromise.then(data => console.log(data));
-
+firstPromise.then((data) => console.log(data));
 
 // Challenge 7
+// We have a API that gets data from a database, it takes an index parameter and returns a promise
+// Your challenge is to use Promise.all to make 3 API calls and return all the data when the calls are complete
 const fakePeople = [
-  { name: 'Rudolph', hasPets: false, currentTemp: 98.6 },
-  { name: 'Zebulon', hasPets: true, currentTemp: 22.6 },
-  { name: 'Harold', hasPets: true, currentTemp: 98.3 },
-]
+  { name: "Rudolph", hasPets: false, currentTemp: 98.6 },
+  { name: "Zebulon", hasPets: true, currentTemp: 22.6 },
+  { name: "Harold", hasPets: true, currentTemp: 98.3 },
+];
 
 const fakeAPICall = (i) => {
   const returnTime = Math.floor(Math.random() * 1000);
@@ -105,7 +102,9 @@ const fakeAPICall = (i) => {
 
 function getAllData() {
   // CODE GOES HERE
-  Promise.all([fakeAPICall(0), fakeAPICall(1), fakeAPICall(2)]).then(data => console.log(data));
+  Promise.all([fakeAPICall(0), fakeAPICall(1), fakeAPICall(2)]).then((data) =>
+    console.log(data)
+  );
 }
 
 getAllData();
